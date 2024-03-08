fab3821c23aa4a5c8110e987fb687503ea5a660c
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-03-08 10:37:27 +0100

    base

---
+++ Project-week1/src/player/MainPlayer.java
@@ -0,0 +1,7 @@
+package player;
+
+public class MainPlayer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Project-week1/src/player/Video.java
@@ -0,0 +1,4 @@
+package player;
+
+public class Video extends ElementoMultimediale{
+}
